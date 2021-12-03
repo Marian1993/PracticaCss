6ef321f67fc48158005c1f8c5fde45d39c45b248
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-12-03 22:20:06 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const validate = require('gulp-w3c-css');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const { series } = require('gulp');
+
+
+function validadorCSS() {
+  gulp.src('./css/*.css')
+    .pipe(validate())
+    .pipe(gulp.dest('./build'));
+}
+
+function watchCSS() {
+  gulp.watch('./css/*.css', validadorCSS);
+}
+
+function watchSass() {
+  gulp.watch("./sass/*.scss", compiladorSass);
+}
+
+function compiladorSass() {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+}
+function comprSass() {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+}
+function comprHtml(){
+  return gulp.src('./html/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist/html'));
+}
+function comprImg(){
+  gulp.src('./img/**/*.jpg')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'));
+}
+
+exports.css = validadorCSS;
+exports.watch = watchCSS;
+exports.compSass = compiladorSass;
+exports.watchSass = watchSass;
+exports.comprCss = comprSass;
+exports.comprHtml = comprHtml;
+exports.comprImg = comprImg;
+exports.all = series(comprSass,comprHtml,comprImg);
+
+
+
